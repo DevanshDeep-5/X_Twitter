8bc2fa9d7348a7434e5520421047d59170ffbb09
DevanshDeep-5 <devanshdeep91@gmail.com>
2024-08-20 12:09:30 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        mycolor: "rgba(33,35,39,1)"
+      }
+    },
+  },
+  plugins: [],
+}
+
